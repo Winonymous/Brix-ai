363030e1e6fd24a0e7f72857e608f2cc3a48d91b
Winonymous <anjuwonololade@gmail.com>
2024-05-29 17:39:03 +0100

    Work by Fire

--- start.py
+++ start.py
@@ -7,12 +7,20 @@ import subprocess
 import time
 
 # import chromadb
+chroma_port = 12345  # Replace with the actual port number
 
-# Start chroma server
-chroma_process = subprocess.Popen(['chroma', 'run', '--path', 'Resource/chroma_db', '--port', '12345' ])
+# Start chroma server# Python code to be executed before starting the chroma process
+python_code = '__import__("pysqlite3"); import sys; sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")'
+
+python_process = subprocess.Popen(['python', '-c', python_code])
+python_process.wait()
+
+# # Start ChromaDB server with the Python code executed first
+command = ['chroma', 'run', '--path', 'Resource/chroma_db', '--port', str(chroma_port)]
+
+chroma_process = subprocess.Popen(command)
 
 # Wait for the localhost server to be ready
-chroma_port = 12345  # Replace with the actual port number
 while True:
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -34,33 +42,4 @@ except KeyboardInterrupt:
     # Terminate subprocesses on Ctrl+C
     chroma_process.terminate()
     gunicorn_process.terminate()
-import socket
-import subprocess
-import time
-
-# Start chroma server
-chroma_process = subprocess.Popen(['chroma', 'run', '--path', 'Resource/chroma_db'])
-
-# Wait for the localhost server to be ready
-chroma_port = 12345  # Replace with the actual port number
-while True:
-    try:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect(('localhost', chroma_port))
-        print("Chroma server is ready")
-        break
-    except ConnectionRefusedError:
-        print("Waiting for Chroma server to start...")
-        time.sleep(1)
-
-# Start gunicorn server
-gunicorn_process = subprocess.Popen(['gunicorn', '-w', '4', '-b', '0.0.0.0:8000', 'main:app'])
-
-# Wait for user interruption
-try:
-    while True:
-        time.sleep(1)
-except KeyboardInterrupt:
-    # Terminate subprocesses on Ctrl+C
-    chroma_process.terminate()
-    gunicorn_process.terminate()
+ 
